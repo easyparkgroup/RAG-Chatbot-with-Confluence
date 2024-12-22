4c0991a557ff5e1a0f49bfc5528ab3bba08db951
Frank Hoffsümmer <frank.hoffsummer@easypark.net>
2024-12-22 08:17:37 +0100

    first stab at last modified

--- src/list_spaces.py
+++ src/list_spaces.py
@@ -1,7 +1,10 @@
 # https://atlassian-python-api.readthedocs.io/confluence.html
 import os
+import time
+from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
 from pathlib import Path
+from threading import Semaphore
 
 import altair as alt
 import pandas as pd
@@ -16,18 +19,24 @@ username = os.getenv("CONFLUENCE_USERNAME")
 api_token = os.getenv("CONFLUENCE_API_KEY")
 base_url = os.getenv("CONFLUENCE_BASE_URL").rstrip("/")
 datadir = Path(__file__).parent.parent / "data"
+pickle_file = Path(datadir / "spaces.pkl")
 
 # Initialize the Confluence client
-confluence = Confluence(url=base_url, username=username, password=api_token)
+ep_confluence = Confluence(url=base_url, username=username, password=api_token)
+
 
 def get_most_recently_modified_page(space_key):
     start = 0
     space_pages = []
     while True:
-        # Retrieve a batch of pages from the space
-        pages = confluence.get_all_pages_from_space(space=space_key, start=start, limit=100, expand="history,version")
+        # Retrieve a batch of pages that belong to the space
+        # or https://atlassian-python-api.readthedocs.io/confluence.html#get-spaces-info
+        # get_space_content()
+        pages = ep_confluence.get_all_pages_from_space(
+            space=space_key, start=start, limit=100, expand="history,version"
+        )
 
-        # Break the loop if no more pages are returned
+        # Break the loop if no more pages are turned
         if not pages:
             break
 
@@ -37,29 +46,28 @@ def get_most_recently_modified_page(space_key):
         start += 100
 
     mod_dates = sorted([p.get("version", {}).get("when") for p in space_pages], reverse=True)
-    dt = datetime.strptime(mod_dates[0], "%Y-%m-%dT%H:%M:%S.%fZ")
-    return dt
+    if mod_dates:
+        dt = datetime.strptime(mod_dates[0], "%Y-%m-%dT%H:%M:%S.%fZ")
+    else:
+        dt = None
+    return space_key, dt
 
 
 def get_all_spaces():
     """Fetch all non-archived spaces from Confluence."""
     start = 0
-    page_size = 100
 
     while True:
-        spaces = confluence.get_all_spaces(start=start, limit=page_size, expand="description.plain")
+        spaces = ep_confluence.get_all_spaces(start=start, limit=100, expand="description.plain")
         for space in spaces["results"]:
             if not space.get("archived", False):
                 space_key = space["key"]
-                # last_modified = get_most_recently_modified_page(space_key)
-
                 result = {
                     "space_key": space_key,
                     "space_name": space["name"],
                     "space_link": f"{base_url}/spaces/{space_key}/pages",
                     "space_type": space["type"],
                 }
-                print(result)
                 yield result
 
         # Break the loop if no more pages are returned
@@ -67,15 +75,32 @@ def get_all_spaces():
             break
 
         # Increment the start index for the next batch
-        start += page_size
+        start += 100
 
 
 def get_data(pf):
     # Load from pickle if it exists, otherwise write to it
     if not pf.exists():
-        all_spaces = list(get_all_spaces())
-        df = pd.DataFrame(all_spaces)
-        df.to_pickle(pf)
+        spaces_df = pd.DataFrame(list(get_all_spaces()))
+        space_keys = spaces_df["space_key"].tolist()
+        rate_limit = 5
+
+        def rate_limited_get_most_recently_modified_page(space_key):
+            with Semaphore(rate_limit):
+                result = get_most_recently_modified_page(space_key)
+                time.sleep(1 / rate_limit)
+                return result
+
+        # get last modified date for all spaces
+        with ThreadPoolExecutor() as executor:
+            last_modified_dates = list(executor.map(rate_limited_get_most_recently_modified_page, space_keys))
+
+        # create a DataFrame from the results
+        lmd = pd.DataFrame(last_modified_dates, columns=["space_key", "last_modified"])
+        # join the last modified dates with the space data
+        df = spaces_df.merge(lmd, on="space_key")
+        # save the data to a pickle file
+        df.to_pickle(pickle_file)
         print("Data written to pickle file.")
     else:
         df = pd.read_pickle(pf)
@@ -83,7 +108,6 @@ def get_data(pf):
     return df
 
 
-pickle_file = Path(datadir / "spaces.pkl")
 df = get_data(pickle_file)
 display_df = df.copy()
 
@@ -108,14 +132,13 @@ with col2:
         alt.Chart(space_type_counts)
         .mark_bar()
         .encode(
-            x=alt.X(field="space_type", type="nominal", title="Space Type", sort="-y"),
-            y=alt.Y(field="count", type="quantitative", title="Count"),
-            color=alt.Color(field="space_type", type="nominal"),
+            x=alt.X(field="space_type", type="nominal", sort="-y"),
+            y=alt.Y(field="count", type="quantitative"),
+            color=alt.Color(field="space_type", type="nominal", legend=None),
             tooltip=["space_type", "count"],
         )
     )
-
-    st.altair_chart(bar_chart)
+    st.altair_chart(bar_chart, use_container_width=True)
 
 st.dataframe(
     display_df,
@@ -132,4 +155,9 @@ st.dataframe(
 )
 
 if __name__ == "__main__":
-    get_data(pickle_file)
+    pd.set_option("display.max_columns", None)
+    pd.set_option("display.width", 1000)
+    pd.set_option("display.max_colwidth", 1000)
+
+    all_spaces = get_data(pickle_file)
+    print(all_spaces.head(25))
